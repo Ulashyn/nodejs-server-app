8011b18d2a6dc8c3526aa137c38975d02adbf1f3
Ulashyn <ulashyn.o@gmail.com>
2024-04-10 02:40:24 +0300

    add edit and delete e2e tests

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -16,5 +16,22 @@ context('Index', () => {
     cy.get('#submit').click();
     cy.url().should('include', '/posts');
   });
+  it('Should edit blog post', () => {
+    cy.visit('/posts');
+    cy.get('.uk-grid').get('.uk-button').contains('Read').first().click();
+    cy.get('.uk-button').first().click();
+    cy.get('#title').clear().type('Cypress test title edited');
+    cy.get('#description').clear().type('Cypress test title edited');
+    cy.get('#content').clear().type('Cypress test title edited (by cypress)');
+    cy.get('#submit').click();
+    cy.url().should('include', '/posts');
+  });
+  it('Should delete blog post', () => {
+    cy.visit('/posts');
+    cy.get('.uk-grid').get('.uk-button').contains('Read').first().click();
+    cy.get('#delete').click();
+    cy.get('.uk-modal-dialog').get('.uk-button').contains('Ok').click();
+    cy.url().should('include', '/posts');
+  });
 
 })
